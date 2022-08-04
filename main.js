5e9d61fd2dbf0df45a943f88d7992bf75a416be8
aldy2002 <97277695+aldy2002@users.noreply.github.com>
2022-08-04 18:58:41 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,59 @@
+let apiUrl = "https://myapi.ruang-ekspresi.id/index.php/";
+
+Vue.createApp({
+    data() {
+      return {
+        message: "Hello Vue!",
+        header: {},
+        articles: []
+      };
+    },
+    methods:{    
+        getHeaderData()
+        {
+          axios
+            .get(
+                "https://raw.githubusercontent.com/aldy2002/tekweb2021/main/assets/js/header.json"
+
+            )
+            .then((res) => {              
+              this.header = res.data;
+              this.getHeaderData();
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        },
+        getAboutData()
+        {
+          axios
+            .get(
+                "https://raw.githubusercontent.com/aldy2002/tekweb2021/main/assets/js/about.json"
+
+            )
+            .then((res) => {              
+              this.about = res.data;
+              this.getAboutData();
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        },
+        getArticlesData()
+        {
+            axios
+            .get(
+              "https://raw.githubusercontent.com/aldy2002/tekweb2021/main/content/articles.json"
+            )
+            .then((res) => {              
+              this.articles = res.data;
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+    },
+    beforeMount() {
+        this.getHeaderData()
+    }
+  }).mount("#app");
